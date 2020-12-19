34f36ab36e57798e8611c2812bcf6df10fe4ae62
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2020-12-20 00:59:20 +0200

    Finish All Code

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 
-import cv2
+import cv2.cv2 as cv2
 import numpy as np
 from PIL import Image
 import os
@@ -13,8 +13,8 @@ detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 # function to get the images and label data
 def getImagesAndLabels(path):
 
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
-    faceSamples=[]
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faceSamples = []
     ids = []
 
     for imagePath in imagePaths:
@@ -25,8 +25,8 @@ def getImagesAndLabels(path):
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
 
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
+        for (x, y, w, h) in faces:
+            faceSamples.append(img_numpy[y:y+h, x:x+w])
             ids.append(id)
 
     return faceSamples,ids
